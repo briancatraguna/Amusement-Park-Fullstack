3f7605c8845995470541ee507c3c33fb57a22199
Brian Catraguna <briancatraguna@rocketmail.com>
2023-04-23 09:19:52 -0400

    Fixed styling

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AuthenticationPage from "./pages/Authentication/index";
-import { Provider, useSelector } from "react-redux";
+import { Provider } from "react-redux";
 import store from "./redux/store";
 import EmployeeHomePage from "./pages/EmployeeHome";
 import HomePage from "./pages/Home";
 import { ROUTES } from "./utils/enums";
 
 function App() {
-
   return (
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route path={ROUTES.auth} element={<AuthenticationPage/>} />
-          <Route path={ROUTES.employee} element={<EmployeeHomePage/>}/>
-          <Route path={ROUTES.home} element={<HomePage/>}/>
+          <Route path={ROUTES.auth} element={<AuthenticationPage />} />
+          <Route path={ROUTES.employee} element={<EmployeeHomePage />} />
+          <Route path={ROUTES.home} element={<HomePage />} />
         </Routes>
       </BrowserRouter>
     </Provider>
