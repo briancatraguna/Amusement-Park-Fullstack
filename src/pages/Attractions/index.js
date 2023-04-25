fa0014016dabc08ae1ef8274617a2c59d07ed020
Brian Catraguna <briancatraguna@rocketmail.com>
2023-04-25 16:11:18 -0400

    Initialize attractions

--- src/pages/Attractions/index.js
+++ src/pages/Attractions/index.js
@@ -1,29 +1,31 @@
 import { Button } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import Header from "../../components/Header";
 import { getAttractions } from "../../utils/api";
 
 const AttractionsPage = () => {
-  const accessToken = useSelector((state) => state.auth.accessToken);
-  const [attractions, setAttractions] = useState([]);
+    const accessToken= useSelector((state) => state.auth.accessToken);
+    const [attractions, setAttractions] = useState({});
 
-  const fetchAttractions = async () => {
-    try {
-      const response = await getAttractions(accessToken);
-      setAttractions(response.data.attractions);
-      console.log(attractions);
-    } catch (error) {
-      alert(error.response.data.message);
-    }
-  };
-  fetchAttractions();
-
-  return (
-    <div>
-      <Header />
-    </div>
-  );
-};
+    useEffect(() => {
+        const fetchAttractions = async() => {
+            try {
+                const response = await getAttractions(accessToken);
+                setAttractions(response.data.attractions);
+            } catch (error) {
+                alert(error.response.data.message);
+            }
+        };
+        fetchAttractions();
+    },[]);
+    
+    
+    return (
+        <div>
+            <Header/>
+        </div>
+    )
+}
 
 export default AttractionsPage;
