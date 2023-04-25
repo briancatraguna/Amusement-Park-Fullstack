9b64c59826cfadf64528663d63fea7eec3cc5229
Brian Catraguna <briancatraguna@rocketmail.com>
2023-04-24 23:21:48 -0400

    Added image in home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,22 @@
 import React from "react";
-import { useSelector } from "react-redux";
 import Header from "../../components/Header";
 import "./style.css";
 
 const HomePage = () => {
-  const accessTokenState = useSelector((state) => state.auth.accessToken);
-
   return (
     <div>
       <Header />
-      <h1>{accessTokenState}</h1>
+      <div className="image-container">
+        <img
+          src={require("../../assets/theme-park-image-home.jpeg")}
+          className="theme-park-image-home"
+          alt="theme park"
+        />
+        <div className="text-container">
+          <h1>Welcome to Voyage of Amusements!</h1>
+          <p>Experience the thrill of the rides and the attractions.</p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,30 +1,36 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from './authSlice';
-import userInfoReducer from './userInfoSlice'
+import authReducer from "./authSlice";
+import userInfoReducer from "./userInfoSlice";
 
 const store = configureStore({
-    reducer: {
-        auth: authReducer,
-        userInfo: userInfoReducer
+  reducer: {
+    auth: authReducer,
+    userInfo: userInfoReducer,
+  },
+  preloadedState: {
+    auth: {
+      accessToken: localStorage.getItem("accessToken"),
+      roleId: localStorage.getItem("roleId"),
     },
-    preloadedState: {
-        auth: {
-            accessToken: localStorage.getItem("accessToken"),
-            roleId: localStorage.getItem("roleId")
-        },
-        userInfo: {
-            user: JSON.parse(localStorage.getItem("user"))
+    userInfo: {
+      user: (() => {
+        try {
+          return JSON.parse(localStorage.getItem("user"));
+        } catch (error) {
+          return null;
         }
-    }
+      })(),
+    },
+  },
 });
 
 store.subscribe(() => {
-    const {accessToken, roleId} = store.getState().auth;
-    localStorage.setItem("accessToken", accessToken);
-    localStorage.setItem("roleId", roleId);
+  const { accessToken, roleId } = store.getState().auth;
+  localStorage.setItem("accessToken", accessToken);
+  localStorage.setItem("roleId", roleId);
 
-    const {user} = store.getState().userInfo;
-    localStorage.setItem("user", user);
-})
+  const { user } = store.getState().userInfo;
+  localStorage.setItem("user", user);
+});
 
 export default store;
