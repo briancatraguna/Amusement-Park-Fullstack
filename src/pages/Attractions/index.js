303410aea78bb493e7170424e4e543bc53f2254c
Brian Catraguna <briancatraguna@rocketmail.com>
2023-04-25 15:31:32 -0400

    Test API call

--- src/pages/Attractions/index.js
+++ src/pages/Attractions/index.js
@@ -1,14 +1,29 @@
-import React from "react";
+import { Button } from "@mui/material";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import Header from "../../components/Header";
+import { getAttractions } from "../../utils/api";
 
 const AttractionsPage = () => {
-    const accessTokenState = useSelector((state) => state.auth.accessToken);
-    return (
-        <div>
-            <Header/>
-        </div>
-    )
-}
+  const accessToken = useSelector((state) => state.auth.accessToken);
+  const [attractions, setAttractions] = useState([]);
+
+  const fetchAttractions = async () => {
+    try {
+      const response = await getAttractions(accessToken);
+      setAttractions(response.data.attractions);
+      console.log(attractions);
+    } catch (error) {
+      alert(error.response.data.message);
+    }
+  };
+  fetchAttractions();
+
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
 
 export default AttractionsPage;
