1efcc6293408e6cf423148274d4808d1c62cc6d1
Brian Catraguna <briancatraguna@rocketmail.com>
2023-04-28 23:00:36 -0400

    Added stores detail page

--- src/pages/StoreDetail/index.js
+++ src/pages/StoreDetail/index.js
@@ -5,6 +5,7 @@ import Header from "../../components/Header";
 import { ROUTES } from "../../utils/enums";
 import { getStoreMenu } from "../../utils/api";
 import "./style.css";
+import { Button } from "@mui/material";
 
 const StoreDetailPage = () => {
   const accessToken = useSelector((state) => state.auth.accessToken);
@@ -22,8 +23,10 @@ const StoreDetailPage = () => {
     setStoreName(storeNameParams);
     const fetchStoreMenu = async () => {
       try {
-        const storeMenuResponse = await getStoreMenu(accessToken, storeIdParams);
-        console.log(storeMenuResponse.data.storeMenu);
+        const storeMenuResponse = await getStoreMenu(
+          accessToken,
+          storeIdParams
+        );
         setStoreMenu(storeMenuResponse.data.storeMenu);
       } catch (error) {
         alert(error.response.data.message);
@@ -35,6 +38,28 @@ const StoreDetailPage = () => {
   return (
     <div>
       <Header />
+      <div className="store-detail-container">
+        <div className="store-info">
+          <h1>{storeName}</h1>
+        </div>
+        <div className="store-menu-container">
+          {storeMenu &&
+            storeMenu.map((menuItem) => (
+              <div className="store-menu-item" key={menuItem.menu_item_id}>
+                <img
+                  src={menuItem.menu_img_url}
+                  alt={menuItem.menu_item_name}
+                />
+                <div>
+                  <h2>{menuItem.menu_item_name}</h2>
+                  <p>{menuItem.item_desc}</p>
+                  <p>{menuItem.item_price}</p>
+                  <Button>Add to Cart</Button>
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
     </div>
   );
 };
